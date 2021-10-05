72c50954f754d75211e9a0b918451c3225d60afa
Alexander C <alexander.culum@gmail.com>
2021-10-05 20:43:04 +0200

    Add Docs.

--- src/test/java/CheckBotTest.java
+++ src/test/java/CheckBotTest.java
@@ -1,4 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CheckBotTest {
+
+    @Test
+    public void testit() {
+        assertEquals(1,1);
+
+    }
 
 
 }
