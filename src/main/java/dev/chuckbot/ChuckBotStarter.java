bb13882b7c39125cfb84e0921bcb4e65ee926d47
LeanderKl <90198306+LeanderKl@users.noreply.github.com>
2021-10-06 14:11:41 +0200

    Implement task-1.

--- src/main/java/dev/chuckbot/ChuckBotStarter.java
+++ src/main/java/dev/chuckbot/ChuckBotStarter.java
@@ -3,7 +3,8 @@ package dev.chuckbot;
 public class ChuckBotStarter {
 
     public static void main(String[] args) {
-
+        JokesContainer jokesContainer = new JokesContainer();
+        jokesContainer.printJokes();
     }
 
 }
