492ec8cf4f7f3db4c0df97696a320a72f530d326
SEA-W6-RR <90201728+SEA-W6-RR@users.noreply.github.com>
2021-10-06 13:35:30 +0200

    add witze.txt

--- src/main/java/dev/chuckbot/ChuckBotStarter.java
+++ src/main/java/dev/chuckbot/ChuckBotStarter.java
@@ -1,8 +1,17 @@
 package dev.chuckbot;
 
+import java.io.File;
+import java.util.List;
+
 public class ChuckBotStarter {
 
     public static void main(String[] args) {
+        for (String argument : args) {
+            System.out.println(argument);
+        }
+        File inputWitze = new File("src/test/resources/witze.txt");
+        // List<String> witzeListe = WitzeImporter.importKunden(inputWitze);
+
 
     }
 
