e7b7268148e34ad6e3f4f432dfa2533d82681744
SEA-W6-RR <90201728+SEA-W6-RR@users.noreply.github.com>
2021-10-13 16:21:22 +0200

    add class Joke + JokeTest and CreationDateComparator + CreationDateComparatorTest

--- src/main/java/dev/chuckbot/Joke.java
+++ src/main/java/dev/chuckbot/Joke.java
@@ -2,16 +2,21 @@ package dev.chuckbot;
 
 import java.time.LocalDate;
 
-public class Joke {
-    private String joke;
+public class Joke{
+    // attributes
+    private String jokeText;
     private LocalDate creationDate;
 
-    public Joke(String joke, LocalDate creationDate) {
-        this.joke = joke;
+    public Joke(String jokeText, LocalDate creationDate) {
+        this.jokeText = jokeText;
         this.creationDate = creationDate;
     }
 
+    public String getJokeText() {
+        return jokeText;
+    }
 
-
-
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
 }
