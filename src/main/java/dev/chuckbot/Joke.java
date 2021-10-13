ef945adcb71723d864797e4cd6dc0e6132d950e1
SEA-W6-RR <90201728+SEA-W6-RR@users.noreply.github.com>
2021-10-13 13:15:27 +0200

    add class Joke + JokeTest

---
+++ src/main/java/dev/chuckbot/Joke.java
@@ -0,0 +1,17 @@
+package dev.chuckbot;
+
+import java.time.LocalDate;
+
+public class Joke {
+    private String joke;
+    private LocalDate creationDate;
+
+    public Joke(String joke, LocalDate creationDate) {
+        this.joke = joke;
+        this.creationDate = creationDate;
+    }
+
+
+
+
+}
---
+++ src/test/java/dev/chuckbot/JokeTest.java
@@ -0,0 +1,15 @@
+package dev.chuckbot;
+
+import org.junit.jupiter.api.Test;
+
+public class JokeTest {
+
+    @Test
+    public void testAddJoke() {
+        // Arrange
+        Joke joke = new Joke("Joke1", 2021-10-12);
+
+    }
+
+
+}
