595cc0e83a6f859c1560ed770a346835b64aa0c8
armanmsb <arman-meshabi-ayandeh_student2021@wilder.school>
2021-10-14 16:28:11 +0200

    Unit tests for Class "ChuckNorrisJokeService"

--- src/test/java/dev/chuckbot/ChuckNorrisJokeServiceTest.java
+++ src/test/java/dev/chuckbot/ChuckNorrisJokeServiceTest.java
@@ -1,32 +1,86 @@
 package dev.chuckbot;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class ChuckNorrisJokeServiceTest {
 
+    private static ChuckNorrisJokeService chuckNorrisJokeService;
+
+    @BeforeAll
+    public static void setUp(){
+
+        JokesPersistence jokesPersistence = new JokesFilePersistence(new File("src/test/resources/jokeServiceTest.txt"));
+        chuckNorrisJokeService = new ChuckNorrisJokeService(jokesPersistence);
+        chuckNorrisJokeService.initialize();
+
+    }
+
     @Test
     public void testInitialize() {
-        File file = new File("src/test/resources/initializeTest.txt");
-        JokesFilePersistence filePersistence = new JokesFilePersistence(file);
-        List<Joke> jokeList  = filePersistence.loadData();
-        assertTrue(jokeList.size()>= 1);
+
+        JokesPersistence jokesPersistence = new JokesFilePersistence(new File("src/test/resources/jokeServiceTest.txt"));
+        chuckNorrisJokeService = new ChuckNorrisJokeService(jokesPersistence);
+        chuckNorrisJokeService.initialize();
+
+        assertTrue(chuckNorrisJokeService.jokeList.size()>= 1);
+
+    }
+
+
+    @Test
+    public void testGetNewestJoke(){
+
+        Joke joke1 = new Joke("Chuck Norris circumcised himself. At birth. With his bare hands.", LocalDate.of(2021,10,14));
+        Joke joke2 = chuckNorrisJokeService.getNewestJoke();
+
+        assertEquals(joke1.getCreationDate(), joke2.getCreationDate());
+        assertEquals(joke1.getJokeText(), joke2.getJokeText());
     }
 
 
     @Test
     public void testAddNewJokes() {
-        File file = new File("src/test/resources/initializeTest.txt");
+
+        JokesFilePersistence filePersistence = new JokesFilePersistence(new File("src/test/resources/jokeServiceTest.txt"));
+        chuckNorrisJokeService = new ChuckNorrisJokeService(filePersistence);
+        chuckNorrisJokeService.initialize();
+        int sizeBeforeAdding= chuckNorrisJokeService.jokeList.size();
+        Joke joke1 = new Joke("Chuck Norris circumcised himself. At birth. With his bare hands.", LocalDate.of(2021,10,14));
+        chuckNorrisJokeService.addNewJoke(joke1);
+        int sizeAfterAdding = chuckNorrisJokeService.jokeList.size();
+
+        assertTrue(sizeAfterAdding>sizeBeforeAdding);
+
     }
 
     @Test
     public void testShutdown() {
 
+        JokesFilePersistence filePersistence = new JokesFilePersistence(new File("src/test/resources/jokeServiceShutdownTest.txt"));
+        chuckNorrisJokeService = new ChuckNorrisJokeService(filePersistence);
+
+        chuckNorrisJokeService.initialize();
+        int sizeBeforeAddingAndShutdown = chuckNorrisJokeService.jokeList.size();
+
+        Joke joke1 = new Joke("In the Beginning, there was nothing. Then Chuck Norris roundhouse kicked nothing and told it to get a job.", LocalDate.of(2019,10,14));
+        chuckNorrisJokeService.addNewJoke(joke1);
+
+        chuckNorrisJokeService.shutdown();
+
+        chuckNorrisJokeService.initialize();
+        int sizeAfterAddingAndShutdown = chuckNorrisJokeService.jokeList.size();
+
+        assertTrue(sizeBeforeAddingAndShutdown<sizeAfterAddingAndShutdown);
     }
 }
