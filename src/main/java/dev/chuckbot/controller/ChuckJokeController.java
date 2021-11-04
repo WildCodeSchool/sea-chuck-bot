a07aa43a16ece733046dc636a5b2485944ae88f4
armanmsb <arman-meshabi-ayandeh_student2021@wilder.school>
2021-11-04 16:17:31 +0100

    Final version of showing List of Jokes

--- src/main/java/dev/chuckbot/controller/ChuckJokeController.java
+++ src/main/java/dev/chuckbot/controller/ChuckJokeController.java
@@ -5,14 +5,12 @@ import dev.chuckbot.JokesFilePersistence;
 import dev.chuckbot.JokesPersistence;
 import dev.chuckbot.service.ChuckNorrisJokeService;
 import dev.chuckbot.util.CreationDateComparator;
-import dev.chuckbot.util.CreationDateComparatorDescending;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -29,7 +27,7 @@ public class ChuckJokeController {
         List<Joke> jokeList = jokeService.getAllJokes();
 
         if(sort.equals("newest")){
-            Collections.sort(jokeList, new CreationDateComparatorDescending());
+            Collections.sort(jokeList, new CreationDateComparator().reversed());
             m.addAttribute("jokes", jokeList);
         }else{
             m.addAttribute("jokes", jokeList);
