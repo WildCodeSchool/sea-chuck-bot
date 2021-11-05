d94a1b8f4e4fb9667cf3e8dcf556b3a0918185fb
Alexander C <alexander.culum@gmail.com>
2021-11-05 13:22:57 +0100

    Add Description

--- src/main/java/dev/chuckbot/JokesFilePersistence.java
+++ src/main/java/dev/chuckbot/JokesFilePersistence.java
@@ -17,6 +17,7 @@ import java.util.List;
 public class JokesFilePersistence implements JokesPersistence {
 
     private final File storage;
+    private final String className = this.getClass().getSimpleName();
 
     public JokesFilePersistence(File storage) {
         this.storage = storage;
@@ -24,6 +25,10 @@ public class JokesFilePersistence implements JokesPersistence {
 
     @Override
     public List<Joke> loadData() {
+
+        int rowsCount = 0;
+        int rowsImported = 0;
+
         //created data structure
         List<Joke> jokeList = new ArrayList<>();
 
@@ -37,34 +42,43 @@ public class JokesFilePersistence implements JokesPersistence {
                 //Create List of Jokes
                 for (String s : inputList) {
                     String[] currentJoke = s.split(";");
+                    rowsCount++;
+
+                    //Check,if the array has at least two colums (joke and date as String)
                     if (currentJoke.length == 2) {
+                        //Check, if the field for the joke is not an emtpy string
                         if (!currentJoke[0].isEmpty()) {
                             DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-                            jokeList.add(new Joke(currentJoke[0], LocalDate.parse(currentJoke[1], dtf)));
+                            //Check, if the field for the date contains a parsable dateformat
+                            // and in case, import both, joke and date, into the list 'jokelist'
+                            if (isParsableToLocaldate(currentJoke[1], dtf)){
+                                jokeList.add(new Joke(currentJoke[0], LocalDate.parse(currentJoke[1], dtf)));
+                                rowsImported++;
+                            }
                         }
                     }
                 }
 
                 //Check if jokesList is empty
                 if (jokeList.isEmpty()) {
-                    System.err.println("File is empty. Couldn't import!");
+                    System.err.println(className + " --- IMPORT FAILED --- No valid entries to import found in source file!");
                     return new ArrayList<>();
                 } else {
-                    System.out.println("import successful");
+                    System.out.println(className + " --- IMPORT SUCCESSFUL --- Imported " + rowsImported + " out of " + rowsCount + " jokes in source file!");
                     return jokeList;
                 }
             } catch (MalformedInputException e) {
-                System.err.println("Wrong file type!");
+                System.err.println(className + " --- IMPORT FAILED --- Wrong file type!");
                 return new ArrayList<>();
             } catch (DateTimeParseException e) {
-                System.err.println("Wrong date format.");
+                System.err.println(className + " --- IMPORT FAILED --- Wrong date format!");
                 return new ArrayList<>();
             } catch (IOException e) {
-                System.err.println("Import error!");
+                System.err.println(className + " --- IMPORT FAILED --- Something went completely wrong!");
                 return new ArrayList<>();
             }
         } else {
-            System.err.println("Import error! File does not exist.");
+            System.err.println(className + " --- IMPORT FAILED --- File does not exist!");
             return new ArrayList<>();
         }
     }
@@ -78,15 +92,25 @@ public class JokesFilePersistence implements JokesPersistence {
                 //writer.append(currJoke.serializedCSV());
                 writer.append(currJoke.getJokeText() + ";" + currJoke.getCreationDate() + "\n");
             }
-            System.out.println("Jokes stored successfully!");
+            System.out.println(className + " --- STORE SUCCESSFUL --- Jokes stored successfully!");
 
         } catch (IOException e) {
-            System.err.println("Tried to export jokes but something went terribly wrong (this is no joke!)");
+            System.err.println(className + " --- STORE FAILED --- Tried to export jokes but something went terribly wrong (this is no joke!)");
         }
     }
 
     @Override
     public void storeDataSmart(List<Joke> jokes) {
-        throw new UnsupportedOperationException("Methode nicht implementiert");
+
+    }
+
+    // Helpermethod to check, wether the string in file could be parsed to localdate or not, based on the specified dateformat
+    private static boolean isParsableToLocaldate(String dateStr, DateTimeFormatter dateFormatter) {
+        try {
+            LocalDate.parse(dateStr, dateFormatter);
+        } catch (DateTimeParseException e) {
+            return false;
+        }
+        return true;
     }
 }
