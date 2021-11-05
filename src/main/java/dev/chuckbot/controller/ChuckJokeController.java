bd1fa7a12fe3f30468e2074643bef24e6c23cdb9
armanmsb <arman-meshabi-ayandeh_student2021@wilder.school>
2021-11-05 11:23:59 +0100

    Final version of showing List of Jokes

--- src/main/java/dev/chuckbot/controller/ChuckJokeController.java
+++ src/main/java/dev/chuckbot/controller/ChuckJokeController.java
@@ -5,9 +5,14 @@ import dev.chuckbot.JokesFilePersistence;
 import dev.chuckbot.JokesPersistence;
 import dev.chuckbot.service.ChuckNorrisJokeService;
 import dev.chuckbot.util.CreationDateComparator;
+import dev.chuckbot.util.JokeTextComparator;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.File;
@@ -18,7 +23,7 @@ import java.util.List;
 public class ChuckJokeController {
 
     @GetMapping("/alljokes")
-    public String joke(Model m, @RequestParam String sort){
+    public String displayAllJoke(Model model, @RequestParam String sort){
 
         File f = new File("src/test/resources/RiBeJokes.txt");
         JokesPersistence jp = new JokesFilePersistence(f);
@@ -26,13 +31,27 @@ public class ChuckJokeController {
         jokeService.initialize();
         List<Joke> jokeList = jokeService.getAllJokes();
 
-        if(sort.equals("newest")){
+        if(sort.equals("date")){
             Collections.sort(jokeList, new CreationDateComparator().reversed());
-            m.addAttribute("jokes", jokeList);
+            model.addAttribute("jokes", jokeList);
+        }else if(sort.equals("alphabetically")){
+            Collections.sort(jokeList, new JokeTextComparator());
+            model.addAttribute("jokes", jokeList);
         }else{
-            m.addAttribute("jokes", jokeList);
+            model.addAttribute("jokes", jokeList);
         }
 
         return "/home.html";
+    }
+
+    @PostMapping(value="/addNewJoke",consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public String addNewJoke(MultiValueMap paramMap){
+        System.out.println(paramMap);
+        return "addNewJokeForm.html";
+    }
+
+    @GetMapping("/addNewJokeForm")
+    public String addNewJoke(){
+        return "addNewJokeForm.html";
     }
 }
