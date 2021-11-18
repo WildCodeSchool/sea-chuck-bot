0a292eaf888b9967fa3228d9dbe262f038c0eb42
David Claybourne <claybourne.david@gmail.com>
2021-11-18 23:37:00 +0100

    change init sql accordingly

--- data/chuckdb/init/init.sql
+++ data/chuckdb/init/init.sql
@@ -5,20 +5,20 @@ SET names 'utf8';
 
 CREATE TABLE joke (
     id int NOT NULL AUTO_INCREMENT,
-    joketext text NOT NULL,
-    creationdate date,
+    joke_text VARCHAR(255) NOT NULL,
+    creation_date date,
     PRIMARY KEY (ID)
 );
 
 # INSERT TEST DATA
 
-INSERT INTO JOKES (joketext, creationdate) VALUES ('Chuck Norris kann Feuer mit einer Lupe machen – nachts!', NOW());
-INSERT INTO JOKES (joketext, creationdate) VALUES ('Chuck Norris läuft 100 Meter in einer Sekunde. Er kennt immer eine Abkürzung.', NOW());
-INSERT INTO JOKES (joketext, creationdate) VALUES ('Chuck Norris kann ein Feuer entfachen, indem er zwei Eiswürfel aneinander reibt.', NOW());
-INSERT INTO JOKES (joketext, creationdate) VALUES ('Chuck Norris wurde gestern geblitzt – beim Einparken.', NOW());
-INSERT INTO JOKES (joketext, creationdate) VALUES ('Wenn man Chuck Norris fragt, wie viele Liegestütze er schafft, antwortet er: "Alle."', NOW());
-INSERT INTO JOKES (joketext, creationdate) VALUES ('Chuck Norris kann ein Fünfeck zeichnen – mit vier Strichen.', NOW());
-INSERT INTO JOKES (joketext, creationdate) VALUES ('Chuck Norris entführt Aliens.', NOW());
-INSERT INTO JOKES (joketext, creationdate) VALUES ('Chuck Norris kann Drehtüren zuknallen.', NOW());
-INSERT INTO JOKES (joketext, creationdate) VALUES ('Chuck Norris kann Atom- von Ökostrom unterscheiden – und zwar am Geschmack.', NOW());
-INSERT INTO JOKES (joketext, creationdate) VALUES ('Chuck Norris hat bis Unendlich gezählt. Zweimal.', NOW());
+INSERT INTO joke (joke_text, creation_date) VALUES ('Chuck Norris kann Feuer mit einer Lupe machen – nachts!', NOW());
+INSERT INTO joke (joke_text, creation_date) VALUES ('Chuck Norris läuft 100 Meter in einer Sekunde. Er kennt immer eine Abkürzung.', NOW());
+INSERT INTO joke (joke_text, creation_date) VALUES ('Chuck Norris kann ein Feuer entfachen, indem er zwei Eiswürfel aneinander reibt.', NOW());
+INSERT INTO joke (joke_text, creation_date) VALUES ('Chuck Norris wurde gestern geblitzt – beim Einparken.', NOW());
+INSERT INTO joke (joke_text, creation_date) VALUES ('Wenn man Chuck Norris fragt, wie viele Liegestütze er schafft, antwortet er: "Alle."', NOW());
+INSERT INTO joke (joke_text, creation_date) VALUES ('Chuck Norris kann ein Fünfeck zeichnen – mit vier Strichen.', NOW());
+INSERT INTO joke (joke_text, creation_date) VALUES ('Chuck Norris entführt Aliens.', NOW());
+INSERT INTO joke (joke_text, creation_date) VALUES ('Chuck Norris kann Drehtüren zuknallen.', NOW());
+INSERT INTO joke (joke_text, creation_date) VALUES ('Chuck Norris kann Atom- von Ökostrom unterscheiden – und zwar am Geschmack.', NOW());
+INSERT INTO joke (joke_text, creation_date) VALUES ('Chuck Norris hat bis Unendlich gezählt. Zweimal.', NOW());
