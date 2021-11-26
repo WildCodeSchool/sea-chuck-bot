2581c085a1114afabdaf06a6811631ba661b66de
David Claybourne <claybourne.david@gmail.com>
2021-11-26 14:38:47 +0100

    Added javascript auth

--- data/hubot/scripts/chuckbot.js
+++ data/hubot/scripts/chuckbot.js
@@ -1,8 +1,13 @@
 module.exports = (chuckbot) => {
     chuckbot.respond(/(whataboutchuck)/gi, (res) => {
-                //wrap the HTTP get call as a Promise
+            //wrap the HTTP get call as a Promise
+            user = process.env.CHUCKBOT_USERNAME
+            pass = process.env.CHUCKBOT_PASSWORD
+            auth = 'Basic ' + new Buffer(user + ':' + pass).toString('base64')
             new Promise((resolve, reject) =>
-                chuckbot.http("http://chuckjokeservice:8080/api/jokes/random").get()((err, response, body) =>
+                chuckbot.http(process.env.CHUCKBOT_URL)
+                    .headers(Authorization: auth, Accept: 'application/json')
+                    .get()((err, response, body) =>
                     err ? reject(err) : resolve(JSON.parse(body).jokeText)
                 )
             )
